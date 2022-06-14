08199d36dc71f49992c02a22daf43f00f3b1ee49
Noice_Anas <anas.alhalabi2002@gmail.com>
2022-06-14 11:10:13 +0300

    Initial Commit

---
+++ PDF creator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDF creator
+//
+//  Created by Noice_anas on 14/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDF creator/PDF_creatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_creatorApp.swift
+//  PDF creator
+//
+//  Created by Noice_anas on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PDF_creatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
